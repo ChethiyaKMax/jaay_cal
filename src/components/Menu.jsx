91a708bb5da93d2984193c882ce18e7f1976bccc
Chethiya Dissanayake <chethiyakdis@gmail.com>
2021-12-12 01:21:30 +0530

    second commit - menu #29

---
+++ src/components/Menu.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import funkyLogo from "../assets/images/menu-logo.png";
+import menuMissions from "../assets/images/menu_missions.png";
+import menuRef from "../assets/images/menu_ref.png";
+import menuRewards from "../assets/images/menu_rewards.png";
+import menuAbout from "../assets/images/menu_about.png";
+import menuClose from "../assets/images/menu_close.png";
+import Referral from "../screens/Referral.jsx";
+
+export default function Menu(props) {
+  return (
+    <div className={props.isOpen ? "menu-wrapper active" : "menu-wrapper"}>
+      <div
+        className={props.isOpen ? "menu-container active" : "menu-container"}
+      >
+        <div className="menu">
+          <img
+            src={menuClose}
+            alt=""
+            className="close-button"
+            onClick={() => props.openMenu()}
+          />
+          <img className="menu-logo" src={funkyLogo} alt="" />
+          <div className="menu-links">
+            <div className="menu-item">
+              <img src={menuMissions} alt="" />
+              <div className="menu-item-text">Missions</div>
+            </div>
+            <div className="menu-item">
+              <img src={menuRef} alt="" />
+              <div className="menu-item-text">Referrals</div>
+            </div>
+            <div className="menu-item">
+              <img src={menuRewards} alt="" />
+              <div className="menu-item-text">My Rewards</div>
+            </div>
+            <div className="menu-item">
+              <img src={menuAbout} alt="" />
+              <div className="menu-item-text">About Funky</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/screens/Referral.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+export default function Referral(props){
+    return(
+        <div className="referral-container"></div>
+    )
+}
